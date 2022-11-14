ffd2677cbeaeffecee276f99b5adb6938fe98e0d
Zaid Jamil <zaid@simplii.net>
2022-11-14 01:53:26 -0700

    clean RN70.5

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+//This file is needed for react-native-victor-icons
+module.exports = {
+    dependencies: {
+      'react-native-vector-icons': {
+        platforms: {
+          ios: null,
+        },
+      },
+    },
+  };
